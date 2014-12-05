39d6cc44079ce575bc0e0cc9dc2a865a63610046
fperraudeau <perraudeau.f@gmail.com>
2014-12-05 07:41:49 -0800

    prediction

--- Lasso_CV.R
+++ Lasso_CV.R
@@ -1,62 +1,111 @@
-setwd("~/Documents/STAT215A/final project")
+setwd("~/Documents/STAT215A/final_project")
 library(glmnet)
 library(doMC)
 library(doParallel)
+library(msgps)
 source("helperFunctions.R")
 load("fMRIdata.RData")
-ls()
 
+###################
+# GLM with Lasso, Ridge and elasticnet regularization
+###################
 
-ShrinkageCV <- function(obs, resp, nfolds, foldid, alpha){
-  registerDoMC(cores=6)
-  cvfit <- cv.glmnet(x = obs, y = resp, family = "mgaussian", nlambda = 100, 
-                     parallel = TRUE, type.measure = "mse", nfolds = nfolds,
-                     foldid = foldid, alpha = alpha)
-  return(cvfit) 
-}
-
-
-ShrinkageResult <- function(obs.train, resp.train, obs.val, resp.val, alpha){
-  ncores <- 6
-  registerDoParallel(ncores)
-  
-  nfolds <- 10
-  foldid <- sample(1:nfolds, size=nrow(resp.train), replace=TRUE)
-  result <- vector()
-  
-  result.loop <- foreach(i = 1:length(alpha)) %dopar% {
-    cvfit <- ShrinkageCV(obs.train, resp.train, nfolds = nfolds, foldid = foldid, 
-                         alpha = alpha[i])
-    lambda.1se <- cvfit$lambda.1se
-    index.lambda.1se <- which(cvfit$lambda %in% cvfit$lambda.1se)
-    pred.err <- c(cvfit$cvm[index.lambda.1se], cvfit$cvsd[index.lambda.1se]) 
-    val.err <- (1 / nrow(obs.val)) * sum(
-      (predict(cvfit, newx = obs.val, s = "lambda.1se")[, ,1] - resp.val)^2)
-    result <- cbind(result, 
-                    c(lambda.1se, pred.err[1], pred.err[2], val.err))
-    return(result)
-  }
-  result <- as.data.frame(result.loop)
-  result <- round(result, 2)
-  colnames(result) <- c("Ridge", "alpha.25", "alpha.5", "alpha.75",
-                        "Lasso")
-  rownames(result) <- c("Lambda 1se", "Mean CV Error", "SD CV Error", "Prediction Error")
-  return(as.data.frame(result))
-}
-
+##### Cross-validation
 obs <- fit_feat
 resp <- resp_dat
+# obs and resp are divided into a training set (80%) and a validation set (20%)
 select.tr <- as.logical(sample(c(1,0), size=nrow(obs), replace=TRUE, prob = c(4/5, 1/5)))
 obs.train <- obs[select.tr, ]
 resp.train <- resp[select.tr, ]
 obs.val <- obs[!select.tr, ]
 resp.val <- resp[!select.tr, ]
 
+# alpha to be tested. alpha = 0 ridge regularization, 
+# alpha = 1 lasso regularization
+#alpha <- c(0, 0.25, 0.5, 0.75, 1)
+#cv.result <- cvResult(obs.train, resp.train, obs.val, resp.val, alpha)
+#write.table(cv.result, "CVresult.csv",  sep = ",", 
+#            row.names = TRUE, col.names = TRUE)
 
-alpha <- c(0, 0.25, 0.5, 0.75, 1)
-shrinkageResult <- ShrinkageResult(obs.train, resp.train, obs.val, resp.val, alpha)
-write.table(shrinkageResult, "shrinkageResult.csv",  sep = ",", 
+
+#### AICc
+# from ridge to lasso
+alpha <- c(0.99, 0.75, 0.5, 0.25, 0)
+AICc.result <- InformationCriterion(obs.train, resp.train, obs.val, resp.val, alpha, 
+                     criterion = "AICc")
+write.table(AICc.result, "AICcResult.csv",  sep = ",", 
             row.names = TRUE, col.names = TRUE)
+
+
+#### BIC
+BIC.result <- InformationCriterion(obs.train, resp.train, obs.val, resp.val, alpha, 
+                                    criterion = "BIC")
+write.table(BIC.result, "BICresult.csv",  sep = ",", 
+            row.names = TRUE, col.names = TRUE)
+
+
+#### To test on small dataset
+obs.train <- obs.train[1:200, 1:500]
+resp.train <- resp.train[1:200, 1:10]
+
+obs.val <- obs.val[1:50, 1:500]
+resp.val <- resp.val[1:50, 1:10]
+
+
+#### ES-CV
+# sequence of lambda to test, allows alignment of the lambda in the folds
+# It indexes the solution path
+lambda <- seq(1, 0, length.out = 100)
+# number of folds
+V <- 10
+# from ridge to lasso
+alpha <- c(0, 0.25, 0.5, 0.75, 1)
+cvESCV.result <- cvESCV(lambda, V, alpha)
+write.table(cvESCV.result, "cvESCV.csv",  sep = ",", 
+            row.names = TRUE, col.names = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
